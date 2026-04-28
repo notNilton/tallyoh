7f487a5c7122da980a7cbf1ddda95a9cb83dcea8
Nilton Naab <nilton.naab@gmail.com>
2026-04-28 11:32:05 -0400

    chore(build): ajustar vite local

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import { devtools } from '@tanstack/devtools-vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
@@ -10,13 +9,11 @@ const config = defineConfig({
     port: 3400,
     proxy: {
       '/api': 'http://localhost:3000',
-      '/auth': 'http://localhost:3000',
       '/users': 'http://localhost:3000',
       '/health': 'http://localhost:3000',
     },
   },
   plugins: [
-    devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackRouter({ target: 'react', autoCodeSplitting: true }),
